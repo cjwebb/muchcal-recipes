18da6188b4a26a7157f4dd24b8ee7917b674d609
cjwebb <colinjameswebb@hotmail.com>
2015-08-23 04:20:49 +0100

    Add CORS headers
    Currently accepts all, so will need to configure at some point.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express      = require('express'),
     expValidator = require('express-validator'),
     bodyParser   = require('body-parser'),
     morgan       = require('morgan'),
+    cors         = require('cors'),
     app          = express();
 
 var routes       = require('./app/routes');
@@ -11,6 +12,7 @@ var routes       = require('./app/routes');
 app.use(morgan('short'));
 app.use(bodyParser.json());
 app.use(expValidator());
+app.use(cors());
 
 app.get('/recipes', routes.recipes.search)
 app.get('/recipes/:id', routes.recipes.get)
